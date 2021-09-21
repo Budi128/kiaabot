96390ca0222b63e655b1399609ba4919ca5a7233
budilutfii <85115264+Budi128@users.noreply.github.com>
2021-09-21 23:50:04 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -1,10 +1,11 @@
 let handler = async (m) => {
+    let user = global.db.data.users[m.sender]
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    m.reply(`${global.db.data.users[who].limit} Limit Left ಥ_ಥ`)
+    m.reply(`*Nama* :${global.db.data.users[who].name}\n*Limit* :${global.db.data.users[who].limit}\n*EXP* :${global.db.data.users[who].exp}\n*Level* :${global.db.data.users[who].level}\n*Rank* :${global.db.data.users[who].role}`)
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
-handler.command = /^(limit)$/i
+handler.command = /^(limit|exp|level|rank)$/i
 module.exports = handler
