8745eabcc564bedce1c110ca422db6009366de4c
budilutfii <85115264+Budi128@users.noreply.github.com>
2021-09-22 15:33:30 +0700

    Update premiummark.js

--- plugins/premiummark.js
+++ plugins/premiummark.js
@@ -1,13 +1,11 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
-
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Terverifikasi Member Premium*', 'status@broadcast')
 }
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = /@6281351687192|@6287837078713/i
+handler.customPrefix=/@6281351687192|@6287837078713/i
+handler.command = new RegExp
 handler.owner = false
 handler.mods = false
 handler.premium = false
