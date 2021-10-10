fe98e55a084c5c0dc2682ce1c3d7ee4a9d9f5bb8
budilutfii <85115264+Budi128@users.noreply.github.com>
2021-10-10 16:32:03 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,19 +1,17 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = function (m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.db.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
+let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i // hanya link grup wa
+//let linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/i // semua jenis link
+handler.before = function (m, { isBotAdmin, isAdmin }) {
+    if (m.isBaileys && m.fromMe) return true
+    let chat = global.DATABASE.data.chats[m.chat]
+    let isLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) {
-    m.reply('Hapus!!\n\nLink Grup terdeteksi')
-    if (global.opts['restrict']) {
-      if (isAdmin || !isBotAdmin) return true
-      // this.groupRemove(m.chat, [m.sender])
+    if (chat.antiLink && isLink && !isAdmin && !m.isBaileys) {
+        conn.reply(m.chat, `kak @${m.chat.split("-")[0]}, ada yang share link`, m, { contextInfo: { mentionedJid: [m.chat.split("-")[0] + '@s.whatsapp.net'] } })
+        if (isAdmin || !isBotAdmin) return true
+        this.groupRemove(m.chat, [m.sender])
     }
-  }
-  return true
+    return true
 }
-
 module.exports = handler
